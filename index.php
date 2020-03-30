7a12a360b36345bd074f50442aadd8c99f345f23
mayard689 <doc-albert@laposte.net>
2020-03-30 13:35:22 +0200

    index modified to make cars and nikes doing some stuff

--- index.php
+++ index.php
@@ -5,8 +5,32 @@ require_once 'Bicycle.php';
 
 
 $myBike=new Bicycle("more or less red");
-echo $myBike->getColor();
-
 $myCar=new Car("blue", 5, "electric");
-echo $myCar->getEnergy();
+$myCar2=new Car("blue", 5, "electric");
+
+$myCar->start();
+$myCar2->start();
+
+?>
+
+<html>
+
+    <?= $myBike->getColor() ?><br>
+    <?= $myBike->forward() ?><br>
+    <?= $myBike->brake() ?><br>
+    
+    
+    <?= $myCar->getEnergy() ?><br>
+    
+    <?= $myCar->forward() ?><br>
+    <?= $myCar->brake() ?><br>
+    
+    
+    <?= $myCar2->getEnergy() ?><br>
+    
+    <?= $myCar2->forward() ?><br>
+    <?= $myCar2->brake() ?><br>
+
+</html>
+
 
